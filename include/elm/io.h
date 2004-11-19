3e0b57b90c3ecd923fe6701b4a3b0dc8838f6a6f
casse <devnull@localhost>
2004-11-19 17:27:44 +0000

    First version.

---
+++ include/elm/io.h
@@ -0,0 +1,10 @@
+/*
+ * $Id$
+ * Copyright (c) 2004, Alfheim Corporation.
+ *
+ * io.h -- formatted IO classes interfaces.
+ */
+
+#include <elm/io/io.h>
+#include <elm/io/OutFileStream.h>
+#include <elm/io/InFileStream.h>
