200d876023a691e85abb0b59c029beca97188502
casse <devnull@localhost>
2006-05-26 09:51:44 +0000

    Added Pair class.

--- include/elm/utility.h
+++ include/elm/utility.h
@@ -1,6 +1,6 @@
 /*
  * $Id$
- * Copyright (c) 2004, Alfheim Corporation.
+ * Copyright (c) 2006 - IRIT-UPS <casse@irit.fr>
  *
  * utility.h -- useful classes interfaces.
  */
@@ -60,112 +60,32 @@ public:
 };
 
 
-// Lock class
-class Lock {
-	int usage;
+// Pair class
+template <class T1, class T2>
+class Pair {
 public:
-	inline Lock(int _usage = 0);
-	virtual ~Lock(void) { };
-	inline void lock(void);
-	inline void unlock();
+	T1 fst;
+	T2 snd;
+	inline Pair(const T1& _fst, const T2& _snd);
+	inline Pair(const Pair<T1, T2>& pair);
+	inline Pair<T1, T2>& operator=(const Pair<T1, T2>& pair);
 };
 
-
-// AutoPtr class
-template <class T> class AutoPtr {
-	Lock *ptr;
-	static inline Lock *null(void);
-public:
-	inline AutoPtr(T *p = 0);
-	inline AutoPtr(const AutoPtr& locked);
-	inline ~AutoPtr(void);
-	inline AutoPtr& operator=(const AutoPtr& locked);
-	inline AutoPtr& operator=(T *p);
-	inline T *operator->(void) const;
-	inline T& operator*(void) const;
-	inline T *operator&(void) const;
-	inline bool isNull(void) const;
-	inline operator bool(void) const { return !isNull(); };
-	inline bool operator==(const AutoPtr<T>& ap) const;
-	inline bool operator!=(const AutoPtr<T>& ap) const;
-	inline bool operator>(const AutoPtr<T>& ap) const;
-	inline bool operator>=(const AutoPtr<T>& ap) const;
-	inline bool operator<(const AutoPtr<T>& ap) const;
-	inline bool operator<=(const AutoPtr<T>& ap) const;
-};
-
-// Lock methods
-Lock::Lock(int _usage): usage(_usage) {
-}
-void Lock::lock(void) {
-	usage++;
-}
-void Lock::unlock() {
-	if(!(--usage))
-		delete this;
+// Pair inlines
+template <class T1, class T2>
+inline Pair<T1, T2>::Pair(const T1& _fst, const T2& _snd): fst(_fst), snd(_snd) {
 }
 
-
-// AutoPtr methods
-template <class T> Lock *AutoPtr<T>::null(void) {
-	static Lock _null(1);
-	return &_null;
-}
-template <class T> AutoPtr<T>::AutoPtr(T *p): ptr(p ? (Lock *)p : null()) {
-	ptr->lock();
-}
-template <class T> AutoPtr<T>::~AutoPtr(void) {
-	ptr->unlock();
-}
-template <class T> T *AutoPtr<T>::operator->(void) const {
-	assert(!isNull());
-	return (T *)ptr;
-}
-template <class T> AutoPtr<T>::AutoPtr(const AutoPtr& locked): ptr(locked.ptr) {
-	ptr->lock();
-}
-template <class T> AutoPtr<T>& AutoPtr<T>::operator=(const AutoPtr& locked) {
-	ptr->unlock();
-	ptr = locked.ptr;
-	ptr->lock();
-	return *this;
-}
-template <class T> AutoPtr<T>& AutoPtr<T>::operator=(T *p) {
-	ptr->unlock();
-	ptr = p ? p : null();
-	ptr->lock();
-	return *this;
-}
-template <class T> bool AutoPtr<T>::isNull(void) const {
-	return ptr == null();
-}
-template <class T> T& AutoPtr<T>::operator*(void) const {
-	assert(!isNull());
-	return *(T *)ptr;
+template <class T1, class T2>
+inline Pair<T1, T2>::Pair(const Pair<T1, T2>& pair)
+: fst(pair.fst), snd(pair.snd) {
 }
 
-template <class T> T *AutoPtr<T>::operator&(void) const {
-	return isNull() ? 0 : (T *)ptr;
+template <class T1, class T2>
+inline Pair<T1, T2>& Pair<T1, T2>::operator=(const Pair<T1, T2>& pair) {
+	fst = pair.fst;
+	snd = pair.snd;
 }
-template <class T> bool AutoPtr<T>::operator==(const AutoPtr<T>& ap) const {
-	return ptr == ap.ptr;
-}
-template <class T> bool AutoPtr<T>::operator!=(const AutoPtr<T>& ap) const {
-	return ptr != ap.ptr;
-}
-template <class T> bool AutoPtr<T>::operator>(const AutoPtr<T>& ap) const {
-	return ptr > ap.ptr;
-}
-template <class T> bool AutoPtr<T>::operator>=(const AutoPtr<T>& ap) const {
-	return ptr >= ap.ptr;
-}
-template <class T> bool AutoPtr<T>::operator<(const AutoPtr<T>& ap) const {
-	return ptr < ap.ptr;
-}
-template <class T> bool AutoPtr<T>::operator<=(const AutoPtr<T>& ap) const {
-	return ptr <= ap.ptr;
-}
-
 
 };	// elm
 
--- src/utility.cpp
+++ src/utility.cpp
@@ -112,4 +112,47 @@ static StringComparator string_comp_obj;
 template <> Comparator<String>& Comparator<String>::def = string_comp_obj;
 
 
+/**
+ * @class Pair
+ * This template class is mainly useful for returning or passing in parameter
+ * a data item composed of a two values.
+ * @param T1	First value type.
+ * @param T2	Second value type.
+ */
+
+
+/**
+ * @var Pair::fst;
+ * The first value of the pair : may be read or written.
+ */
+
+
+/**
+ * @var Pair::snd;
+ * The second value of the pair : may be read or written.
+ */
+
+
+/**
+ * @fn Pair::Pair(const T1& _fst, const T2& _snd);
+ * Build a pair from two values.
+ * @param _fst	First value.
+ * @param _snd	Second value.
+ */
+
+
+/**
+ * @fn Pair::Pair(const Pair<T1, T2>& pair);
+ * Build a pair by copying another pair.
+ * @param pair	Pair to copy.
+ */
+
+
+/**
+ * @fn Pair<T1, T2>& Pair::operator=(const Pair<T1, T2>& pair);
+ * Assignment overload for pairs.
+ * @param pair	Pair to assign.
+ * @return		Current pair.
+ */
+
 };
