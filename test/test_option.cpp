ad824b37d4044c16b4c93b79131a0c6538100e81
casse <devnull@localhost>
2006-07-14 12:32:05 +0000

    Added option::EnumOption class.

--- test/test_option.cpp
+++ test/test_option.cpp
@@ -13,19 +13,58 @@ using namespace elm::option;
 
 Manager man;
 
+// EnumOption test
+typedef enum val_t {
+	one,
+	two,
+	three
+} val_t;
+EnumOption<int>::value_t vals[] = {
+	{ "value", one },
+	{ "one", one },
+	{ "two", two },
+	{ "three", three },
+	{ "", one }
+};
+EnumOption<int> enum_opt(man, 'e', "enum", "", vals);
+
+
 // test_option()
 void test_option(void) {
 	CHECK_BEGIN("Option");
 
-	// Unknown option
-	{
-		char *argv[] = {
-			"command",
-			"--bad",
-			0
+	try {
+		// Unknown option
+		{
+			char *argv[] = { "command", "--bad", 0 };
+			CHECK_EXCEPTION(OptionException, man.parse(2, argv));
 		};
-		CHECK_EXCEPTION(OptionException, man.parse(2, argv));
-	};
+
+		// EnumOption test
+		{
+			CHECK(enum_opt == one);
+			enum_opt = two;
+			CHECK(enum_opt == two);
+			{
+				char *argv[] = { "command", "-e", "one", 0 };
+				man.parse(3, argv);
+				CHECK(enum_opt == one);
+			}
+			{
+				char *argv[] = { "command", "-e", "two", 0 };
+				man.parse(3, argv);
+				CHECK(enum_opt == two);
+			}
+			{
+				char *argv[] = { "command", "-e", "four", 0 };
+				CHECK_EXCEPTION(OptionException, man.parse(3, argv));
+			}
+		}
+	}
+	catch(OptionException& e) {
+		__case.failed();
+		cerr << "FAILURE: " << e.message() << io::endl;
+	}
 
 	CHECK_END
 }
