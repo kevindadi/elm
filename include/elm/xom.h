37fbc5df7ab887982cdb06923ed0e1fce41ec7da
casse <devnull@localhost>
2006-09-19 13:09:19 +0000

    Added xom.h.

---
+++ include/elm/xom.h
@@ -0,0 +1,20 @@
+/*
+ * $Id$
+ * Copyright (c) 2006, IRIT UPS.
+ *
+ * elm/xom.h -- xom module header.
+ */
+#ifndef ELM_XOM_H
+#define ELM_XOM_H
+
+#include <elm/xom/Builder.h>
+#include <elm/xom/Document.h>
+#include <elm/xom/Element.h>
+#include <elm/xom/Elements.h>
+#include <elm/xom/Node.h>
+#include <elm/xom/NodeFactory.h>
+#include <elm/xom/ParentNode.h>
+#include <elm/xom/String.h>
+#include <elm/xom/Text.h>
+
+#endif // ELM_XOM_H
