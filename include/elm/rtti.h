f1a6115da1e8905458dd29acbfa5dd499db38d60
casse <devnull@localhost>
2006-12-05 22:50:52 +0000

    Added type_info<T>::is_scalar information.

--- include/elm/rtti.h
+++ include/elm/rtti.h
@@ -59,58 +59,66 @@ inline DefaultField<T> field(CString name, T& value, const T& def) {
 // istype trait
 template <class T> struct type_info {
 	enum { is_type = 0 };
+	enum { is_scalar = 0 };
 	enum { is_enum = 0 };
 	enum { is_class = 1 };
 	static inline CString name(void) { return T::_class.name(); }
 };
 typedef struct type_t {
 	enum { is_type = 1 };
+	enum { is_scalar = 0 };
 	enum { is_enum = 0 };
 	enum { is_class = 0 };
 } type_t;
-template <> struct type_info<bool>: public type_t {
+typedef struct scalar_t {
+	enum { is_type = 1 };
+	enum { is_scalar = 1 };
+	enum { is_enum = 0 };
+	enum { is_class = 0 };
+} scalar_t;
+template <> struct type_info<bool>: public scalar_t {
 	static inline CString name(void) { return "<bool>"; }		
 };
-template <> struct type_info<signed int>: public type_t {
+template <> struct type_info<signed int>: public scalar_t {
 	static inline CString name(void) { return "int"; }
 };
-template <> struct type_info<unsigned int>: public type_t {
+template <> struct type_info<unsigned int>: public scalar_t {
 	static inline CString name(void) { return "<unsigned>"; }
 };
-template <> struct type_info<char>: public type_t {
+template <> struct type_info<char>: public scalar_t {
 	static inline CString name(void) { return "<char>"; }
 };
-template <> struct type_info<signed char>: public type_t {
+template <> struct type_info<signed char>: public scalar_t {
 	static inline CString name(void) { return "<signed char>"; }
 };
-template <> struct type_info<unsigned char>: public type_t {
+template <> struct type_info<unsigned char>: public scalar_t {
 	static inline CString name(void) { return "<unsigned char>"; }
 };
-template <> struct type_info<signed short>: public type_t {
+template <> struct type_info<signed short>: public scalar_t {
 	static inline CString name(void) { return "<short>"; }
 };
-template <> struct type_info<unsigned short>: public type_t {
+template <> struct type_info<unsigned short>: public scalar_t {
 	static inline CString name(void) { return "<unsigned short>"; }
 };
-template <> struct type_info<signed long>: public type_t {
+template <> struct type_info<signed long>: public scalar_t {
 	static inline CString name(void) { return "<long>"; }
 };
-template <> struct type_info<unsigned long>: public type_t {
+template <> struct type_info<unsigned long>: public scalar_t {
 	static inline CString name(void) { return "<unsigned long>"; }
 };
-template <> struct type_info<signed long long>: public type_t {
+template <> struct type_info<signed long long>: public scalar_t {
 	static inline CString name(void) { return "<long long>"; }
 };
-template <> struct type_info<unsigned long long>: public type_t {
+template <> struct type_info<unsigned long long>: public scalar_t {
 	static inline CString name(void) { return "<unsigned long long>"; }
 };
-template <> struct type_info<float>: public type_t {
+template <> struct type_info<float>: public scalar_t {
 	static inline CString name(void) { return "<float>"; }
 };
-template <> struct type_info<double>: public type_t {
+template <> struct type_info<double>: public scalar_t {
 	static inline CString name(void) { return "<double>"; }
 };
-template <> struct type_info<long double>: public type_t {
+template <> struct type_info<long double>: public scalar_t {
 	static inline CString name(void) { return "<long double>"; }
 };
 template <> struct type_info<CString>: public type_t {
