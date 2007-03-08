91bddd1c1c29d93a39022726212a877666cdd6d9
casse <devnull@localhost>
2007-03-08 12:55:53 +0000

    Added crash handling facility.

---
+++ include/elm/debug/CrashHandler.h
@@ -0,0 +1,33 @@
+/*
+ * $Id$
+ * Copyright (c) 2007, IRIT - UPS
+ *
+ * CrashHandler class interface
+ */
+#ifndef ELM_DEBUG_CRASH_HANDLER_H
+#define ELM_DEBUG_CRASH_HANDLER_H
+
+namespace elm {
+	
+// CrashHandler class
+class CrashHandler {
+public:
+	static void set(CrashHandler *handler);
+	static inline CrashHandler *get(void) { return current_handler; }
+	static void crash(void);
+	static CrashHandler DEFAULT;
+
+	virtual void setup(void);
+	virtual void handle(void);
+	virtual void cleanup(void);
+
+protected:
+	inline CrashHandler(void) { }
+
+private:
+	static CrashHandler *current_handler; 
+};
+
+} // elm
+
+#endif // ELM_DEBUG_CRASH_HANDLER_H
