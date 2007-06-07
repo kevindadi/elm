d6810aded23b18a350128ae4b783080f6ee17657
ballabri <devnull@localhost>
2007-06-07 07:27:30 +0000

    Added DAGNode structure.

---
+++ include/elm/genstruct/DAGNode.h
@@ -0,0 +1,121 @@
+/*
+ * $Id$
+ * Copyright (c) 2005, IRIT-UPS.
+ *
+ * DAGNode.h -- DAG interface.
+ */
+#ifndef ELM_GENSTRUCT_DAGNODE_H
+#define ELM_GENSTRUCT_DAGNODE_H
+
+#include <elm/assert.h>
+#include <elm/Iterator.h>
+#include <elm/genstruct/SLList.h>
+#include <elm/genstruct/HashTable.h>
+
+namespace elm { namespace genstruct {
+
+// DAGNode class
+template <class T> class DAGNode {
+
+	T value;
+	SLList<DAGNode*> children; 
+	bool deleted;
+
+	
+public:
+
+	// Constructors / Destructors
+	inline DAGNode(T& _value);	
+	inline ~DAGNode(); 
+
+	// Mutators
+	inline void addChild(DAGNode *child);
+	inline void delChild(DAGNode *child);	
+	
+	// Accessors
+	inline T& useValue(void);
+	inline bool isDeleted(void);
+
+	// Iterator class
+	class Iterator: public PreIterator<Iterator, DAGNode*> {
+		friend class DAGNode;
+		
+		typename SLList<DAGNode*>::Iterator iter;
+		inline void skipDeleted();
+	public:
+		inline Iterator(const DAGNode& _node);
+		inline Iterator(const Iterator& source);
+		
+		inline void operator=(const Iterator& source);
+		inline bool ended(void) const;
+		inline DAGNode* item(void) const;
+		inline void next(void);
+	};
+
+
+};
+
+template <class T>
+inline T& DAGNode<T>::useValue(void) {
+	return value;
+}
+
+template <class T>
+inline DAGNode<T>::DAGNode(T& _value) : value(_value), deleted(false) {
+
+}
+
+template <class T>
+inline DAGNode<T>::~DAGNode() {
+}
+
+template <class T>
+inline bool DAGNode<T>::isDeleted() {
+	return(deleted);
+}
+
+template <class T>
+inline void DAGNode<T>::addChild(DAGNode *child) {
+	this->children.addFirst(child);
+}
+
+template <class T>
+inline void DAGNode<T>::delChild(DAGNode *child) {
+	child->deleted = true;
+	this->children.remove(child);
+}
+
+// DAGNode::Iterator class
+template <class T> inline DAGNode<T>::Iterator::Iterator(const DAGNode& _node)
+: iter(typename SLList<DAGNode*>::Iterator(_node.children)) {
+	skipDeleted();	
+}
+
+template <class T> inline DAGNode<T>::Iterator::Iterator(const Iterator& source)
+: iter(source.iter) {
+}
+
+template <class T> inline void DAGNode<T>::Iterator::operator=(const Iterator& source)
+{
+	iter = source.iter;
+}
+template <class T> inline bool DAGNode<T>::Iterator::ended(void) const {
+	return iter.ended();
+}
+template <class T> inline DAGNode<T>* DAGNode<T>::Iterator::item(void) const {
+	return iter.item();
+}
+template <class T> inline void DAGNode<T>::Iterator::next(void) {
+	iter.next();
+	skipDeleted();
+}
+
+template <class T> inline void DAGNode<T>::Iterator::skipDeleted(void) {
+	while ((!iter.ended()) && (iter.item()->deleted) ) {
+		iter.next();
+	}
+}
+
+} } // elm::genstruct
+
+#endif // ELM_GENSTRUCT_DAGNODE_H
---
+++ src/genstruct_DAGNode.cpp
@@ -0,0 +1,22 @@
+/*
+ * $Id$
+ * Copyright (c) 2004, IRIT-UPS.
+ *
+ * src/genstruct_DAGNode.cpp -- DAGNode class implementation.
+ */
+
+#include <elm/genstruct/DAGNode.h>
+
+namespace elm { namespace genstruct {
+
+
+/**
+ * @class DAGNode <elm/genstruct/DAGNode.h>
+ * This class provides a generic implementation of the DAGNode.
+
+ * @param T	Type of data stored in the DAGNode.
+ */
+
+
+
+} } // elm::genstruct
