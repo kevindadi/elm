a62b87a9e52b9395ac68dcb9773ff117dd1aea5d
casse <devnull@localhost>
2007-08-21 09:23:32 +0000

    Fixed documentation. Added GPL header.

--- src/util_BitVector.cpp
+++ src/util_BitVector.cpp
@@ -1,8 +1,23 @@
 /*
- * $Id$
- * Copyright (c) 2005, IRIT UPS.
+ *	$Id$
+ *	BitVector class implementation
  *
- * src/util_BitVector.cpp -- BitVector class implementation.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2005-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <elm/util/BitVector.h>
@@ -11,6 +26,7 @@ namespace elm {
 
 /**
  * Class for building the bit counter array.
+ * @ingroup utility
  */
 class BitCounter {
 public:
@@ -33,6 +49,7 @@ static BitCounter bit_counter;
  * way.</p>
  * <p>Notice that vector is represented as a contiguous block of memory. This
  * bit vector representation is clearly not performant for sparse vectors.</p>
+ * @ingroup utility
  */
 
 
--- src/util_Initializer.cpp
+++ src/util_Initializer.cpp
@@ -1,8 +1,23 @@
 /*
- * $Id$
- * Copyright (c) 2006 - IRIT-UPS <casse@irit.fr>
+ *	$Id$
+ *	Initializer class implementation
  *
- * Initializer class implementation.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2006-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <elm/util/Initializer.h>
@@ -32,6 +47,7 @@ namespace elm {
  * initialization stage.
  * 
  * @param T	Type of the objects to give an order to.
+ * @ingroup utility
  */
 
 
