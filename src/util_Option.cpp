e4acaca994c5de71d607d3449c910023adc1bc65
casse <devnull@localhost>
2007-08-21 09:23:31 +0000

    Fixed documentation. Added GPL header.

--- src/util_Option.cpp
+++ src/util_Option.cpp
@@ -1,8 +1,23 @@
 /*
- * $Id$
- * Copyright (c) 2006, IRIT-UPS <casse@irit.fr>.
+ *	$Id$
+ *	Option class implementation
  *
- * elm/util/Option.h -- implementation of Option class.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2006-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <elm/util/Option.h>
@@ -10,7 +25,7 @@
 namespace elm {
 
 /**
- * @class  <elm/util/Option.h>
+ * @class Option
  * This class is very handly to manage parameters or return value that may be
  * optional. This class records there is no value or there is a value and the
  * instance of the value.
@@ -33,6 +48,7 @@ namespace elm {
  * 			x += val;
  * @endcode
  * @param T	Type of the stored value.
+ * @ingroup utility
  */
 
 
