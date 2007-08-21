9792f18264c3a3770143e0c7240b1b4def93e2f7
casse <devnull@localhost>
2007-08-21 09:22:16 +0000

    Fixed documentation. Added GPL header.

--- src/string_AutoString.cpp
+++ src/string_AutoString.cpp
@@ -1,13 +1,35 @@
 /*
- * $Id$
- * Copyright (c) 2007, IRIT - UPS
+ *	$Id$
+ *	AutoString class interface
  *
- * autostr implementation
+ *	This file is part of OTAWA
+ *	Copyright (c) 2007, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <elm/string/AutoString.h>
 
 namespace elm {
+
+/**
+ * Refer to @ref elm::_.
+ * @ingroup string
+ */
+AutoStringStartup autostr;
+
 
 /**
  * This little object make easier the construction of strings. There is no more
@@ -30,8 +52,8 @@ namespace elm {
  *		call(_ << "Hello world, " << i << " times !");
  * @endcode
  * 
+ * @ingroup string
  */
-AutoStringStartup autostr;
 AutoStringStartup &_ = autostr;
 
 } // elm
