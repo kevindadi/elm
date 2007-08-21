f5833d3c4bd49222b4baabb1d5fdeba636d32f32
casse <devnull@localhost>
2007-08-21 09:20:10 +0000

    Fixed documentation. Added GPL header.

--- src/io_OutFileStream.cpp
+++ src/io_OutFileStream.cpp
@@ -1,8 +1,23 @@
 /*
- * $Id$
- * Copyright (c) 2004, Alfheim Corporation.
+ *	$Id$
+ *	OutFileStream class implementation
  *
- * elm/io/OutFileStream.h -- out file stream implementation.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2004-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <sys/types.h>
@@ -14,7 +29,9 @@
 namespace elm { namespace io {
 
 /**
+ * @class OutFileStreal
  * Stream for outputting to a file.
+ * @ingroup ios
  */
 
 /**
