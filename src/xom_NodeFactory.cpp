39ce48cbb7f781ae99a1d3f5b85da28cf7c83d98
casse <devnull@localhost>
2007-08-21 09:24:10 +0000

    Fixed documentation. Added GPL header.

--- src/xom_NodeFactory.cpp
+++ src/xom_NodeFactory.cpp
@@ -1,8 +1,23 @@
 /*
- * $Id$
- * Copyright (c) 2006, IRIT UPS.
+ *	$Id$
+ *	xom::NodeFactory class interface
  *
- * src/xom_NodeFactory.cpp -- XOM NodeFactory class implementation.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2006-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <elm/assert.h>
@@ -15,8 +30,9 @@
 namespace elm { namespace xom {
 
 /**
- * @class NodeFactory NodeFactory.h "elm/xom.h"
+ * @class NodeFactory
  * Used for building nodes during a load.
+ * @ingroup xom
  */
 
 
