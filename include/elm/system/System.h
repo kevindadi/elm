cde123a1946a96b3ab7bafc1d78294e8f5a6c056
casse <devnull@localhost>
2007-08-21 09:13:55 +0000

    Added System::random() and GPL header.

--- include/elm/system/System.h
+++ include/elm/system/System.h
@@ -1,8 +1,23 @@
 /*
- * $Id$
- * Copyright (c) 2006, IRIT-UPS.
+ *	$Id$
+ *	System class interface
  *
- * elm/system/System.h -- System class interface.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2006-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef ELM_SYSTEM_SYSTEM_H
 #define ELM_SYSTEM_SYSTEM_H
@@ -32,6 +47,7 @@ public:
 class System {
 public:
 	static Pair<PipeInStream *, PipeOutStream *> pipe(void);
+	static unsigned int random(unsigned int top);
 };
 
 } } // elm::system
