d07285b196c6e98b293a5ebeb4cb96abbd89f3a8
casse <devnull@localhost>
2007-08-21 09:23:55 +0000

    Fixed documentation. Added GPL header.

--- src/utility.cpp
+++ src/utility.cpp
@@ -1,54 +1,63 @@
 /*
- * $Id$
- * Copyright (c) 2004, Alfheim Corporation.
+ *	$Id$
+ *	utility module
  *
- * collection.cc -- collection classes implementation.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2004-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <elm/utility.h>
 
 namespace elm {
 
-// int_comp
-class IntComparator: public Comparator<int> {
-	virtual int compare(int v1, int v2) {
-		return v1 > v2 ? 1 : (v1 == v2 ? 0 : -1);
-	};
-};
-static IntComparator int_comp_obj;
-template <> Comparator<int>& Comparator<int>::def = int_comp_obj;
-
-// pointer_comp
-class PointerComparator: public Comparator<void *> {
-	virtual int compare(void *v1, void *v2) {
-		return v1 > v2 ? 1 : (v1 == v2 ? 0 : -1);
-	};
-};
-static PointerComparator pointer_comp_obj;
-template <> Comparator<void *>& Comparator<void *>::def = pointer_comp_obj;
-
-// cstring_comp
-class CStringComparator: public Comparator<CString> {
-	virtual int compare(CString v1, CString v2) {
-		return v1.compare(v2);
-	};
-};
-static CStringComparator cstring_comp_obj;
-template <> Comparator<CString>& Comparator<CString>::def = cstring_comp_obj;
-
-// string_comp
-class StringComparator: public Comparator<String> {
-	virtual int compare(String v1, String v2) {
-		return v1.compare(v2);
-	};
-};
-static StringComparator string_comp_obj;
-template <> Comparator<String>& Comparator<String>::def = string_comp_obj;
-
+/**
+ * @defgroup utility Utility Classes
+ * 
+ * This module contains classes providing miscalleneous services.
+ * 
+ * @li @ref AutoComparator -- implementation of the @ref concept::Comparator
+ * using the default "==" and "<" operators.
+ * @li @ref BitVector -- compact and fast representation of vector of bits,
+ * @li @ref HashKey -- implementation of the @ref concept::Hash concept,
+ * @li @ref Initializer -- framework for single-dependency initialization of
+ * static objects,
+ * @li @ref STRONG_TYPE -- automatic class wrapper around scalar types like
+ * enumeration (to avoid ambiguities in C++ type conversion),
+ * @li @ref VarArg -- wrapper around the variable arguments of function,
+ * @li @ref Version -- unified version representation.
+ * 
+ * Other classes provides base classes to a unified exception system:
+ * @li @ref Exception -- base class of exception,
+ * @li @ref MessageException -- exception with built-in string message.
+ * 
+ * Some classes makes easier the management of structured values:
+ * @li @ref Option -- optional value,
+ * @li @ref Pair -- pair of values,
+ * 
+ * Finally, some classes provides facilities to handle references:
+ * @li @ref AutoPtr and @ref Lock -- provides an automatic pointer management
+ * system,
+ * @li @ref Ref -- mutable references.
+ */
 
 /**
- * @class Exception <elm/utility.h>
+ * @class Exception
  * The base class of exceptions in Elm.
+ * @ingroup utility
  */
 
 
