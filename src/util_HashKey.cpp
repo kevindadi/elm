cf30c7aab86f054a03c3c75d339aecc818435cdd
casse <devnull@localhost>
2007-08-21 09:23:31 +0000

    Fixed documentation. Added GPL header.

--- src/util_HashKey.cpp
+++ src/util_HashKey.cpp
@@ -1,8 +1,23 @@
 /*
- * $Id$
- * Copyright (c) 2006 - IRIT-UPS <casse@irit.fr>
+ *	$Id$
+ *	HashKey class implementation
  *
- * HashKey class implementation.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2006-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <elm/util/HashKey.h>
@@ -16,6 +31,8 @@ namespace elm {
  * hash functions for curren types like integers, void pointer or strings.
  * 
  * Refer to @ref hash concept for more details.
+ * 
+ * @ingroup utility
  */ 
 
 
