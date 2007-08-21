13367a51d480ded61964c3c090fae40a8310f34b
casse <devnull@localhost>
2007-08-21 09:16:21 +0000

    Fixed documentation. Added GPL header.

--- src/genstruct_HashTable.cpp
+++ src/genstruct_HashTable.cpp
@@ -1,8 +1,23 @@
 /*
- * $Id$
- * Copyright (c) 2004, IRIT-UPS.
+ *	$Id$
+ *	HashTable class implementation
  *
- * src/genstruct_HashTable.cpp -- HashTable class implementation.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2004-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <elm/genstruct/HashTable.h>
@@ -18,6 +33,7 @@ namespace elm { namespace genstruct {
  * @param K	Type of the keys.
  * @param T	Type of stored data.
  * @param H	Class implementing @ref hash concept (default to HashKey).
+ * @ingroup gen_datastruct
  */
 
 
