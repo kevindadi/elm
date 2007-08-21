48fdd945c1df09975320f5e82a0e06b5f5b0a82f
casse <devnull@localhost>
2007-08-21 09:15:30 +0000

    Added GPL header.

--- src/genstruct_DLList.cpp
+++ src/genstruct_DLList.cpp
@@ -1,8 +1,23 @@
 /*
- * $Id$
- * Copyright (c) 2004, IRIT-UPS.
+ *	$Id$
+ *	DLList class implementation
  *
- * src/genstruct_DLList.cpp -- DLList class implementation.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2004-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <elm/genstruct/DLList.h>
@@ -14,6 +29,7 @@ namespace elm { namespace genstruct {
  * A generic implementation of a double-link list. It may be traversed using
  * the @ref Iterator class or modified using @ref Editor class.
  * @param T	Type of items stored in the list.
+ * @ingroup gen_datastruct
  */
 
 
