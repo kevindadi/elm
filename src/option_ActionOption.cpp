836e11ab3b0cd49f5c45c29beb03cb8b5c37dff0
casse <devnull@localhost>
2007-08-21 09:21:31 +0000

    Fixed documentation. Added GPL header.

--- src/option_ActionOption.cpp
+++ src/option_ActionOption.cpp
@@ -1,8 +1,23 @@
 /*
- * $Id$
- * Copyright (c) 2005, IRIT UPS.
+ *	$Id$
+ *	ActionOption class implementation
  *
- * elm/option/option_ActionOption.cpp -- ActionOption class implementation.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2005-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <elm/assert.h>
@@ -16,6 +31,7 @@ namespace elm { namespace option {
  * This kind of option is used for performing an action when the option is
  * encountered in the command line option. It must be first derived to
  * assign a body to the @ref perform() method before to be used.
+ * @ingroup options
  */
 
 /**
