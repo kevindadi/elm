282ce5988325c506543ed93a17efc743fe164258
casse <devnull@localhost>
2007-12-19 10:09:34 +0000

    Added XXXStream::null.

--- include/elm/io/OutStream.h
+++ include/elm/io/OutStream.h
@@ -1,8 +1,23 @@
 /*
- * $Id$
- * Copyright (c) 2004, Alfheim Corporation.
+ *	$Id$
+ *	OutStream class interface
  *
- * stream.h -- abstract stream classes interfaces.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2004-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef ELM_IO_OUTSTREAM_H
 #define ELM_IO_OUTSTREAM_H
@@ -19,12 +34,9 @@ public:
 	virtual int write(char byte);
 	virtual int flush(void) = 0;
 	virtual CString lastErrorMessage(void);
+	
+	static OutStream& null;
 };
-
-// Standard IO streams	
-/*extern OutStream& stdout;
-extern OutStream& stderr;*/	
-
 
 } }	// elm::io
 
