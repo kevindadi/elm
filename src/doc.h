5de531f4bd8b251c7fa20d3ae7d1932287972924
casse <devnull@localhost>
2008-01-02 21:40:22 +0000

    Added doc.h containing main documentation.

---
+++ src/doc.h
@@ -0,0 +1,39 @@
+/*
+ *	$Id$
+ *	Main documentation
+ *
+ *	This file is part of OTAWA
+ *	Copyright (c) 2008, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifndef ELM_DOC_H_
+#define ELM_DOC_H_
+
+/**
+ * @mainpage
+ * 
+ * ELM is a multi-purpose library used by the OTAWA project (http://www.otawa.fr).
+ * It provides :
+ * @li generic data structures,
+ * @li generic interface with te operating system (files, command line, etc),
+ * @li a checked plugin system,
+ * @li efficient string class,
+ * @li other utility classes.
+ * 
+ * Please take a look at the module part for detailed description.
+ */
+
+#endif /*ELM_DOC_H_*/
