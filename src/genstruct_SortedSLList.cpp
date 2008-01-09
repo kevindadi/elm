c17149c1fcc5154e9c8814e3224268dc4fa0c1ec
casse <devnull@localhost>
2008-01-09 21:03:35 +0000

    New SortedSLList implementing concepts.

--- src/genstruct_SortedSLList.cpp
+++ src/genstruct_SortedSLList.cpp
@@ -3,7 +3,7 @@
  *	SortedSLList class implementation
  *
  *	This file is part of OTAWA
- *	Copyright (c) 2007, IRIT UPS.
+ *	Copyright (c) 2007-08, IRIT UPS.
  * 
  *	OTAWA is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -26,17 +26,26 @@ namespace elm { namespace genstruct {
 
 
 /**
- * @class SortedSLList <elm/genstruct/SortedSLList.h>
+ * @class SortedSLList
+ * @author C. Ballabriga <ballabri@irit.fr>
  * This class provides a generic implementation of single-link lists.
  * To traverse, you ay use its @ref Iterator and @ref Editor may be used to
  * modify it.
+ * 
+ * @par Implemented concepts:
+ * @ref elm::concept::Collection
+ * @ref elm::concept::MutableCollection
+ * @ref elm::concept::List
+ * 
  * @param T	Type of data stored in the list.
+ * @param R	Comparator of the stored items (must implement the concept
+ * 			@ref elm::concept::Comparator).
  * @ingroup gen_datastruct
  */
 
 
 /**
- * @fn T SortedSLList::first(void) const;
+ * @fn const T& SortedSLList::first(void) const;
  * Get the first item of the list.
  * @return	First item.
  * @warning	It is an error to call this method if the list is empty.
@@ -44,7 +53,7 @@ namespace elm { namespace genstruct {
 
 
 /**
- * @fn T SortedSLList::last(void) const;
+ * @fn const T& SortedSLList::last(void) const;
  * Get the last item of the list. Remark that this method is really inefficient.
  * Its working time is in O(n), n number of nodes in the list. Use it only with
  * small list or revert to more powerful data structures.
@@ -68,14 +77,14 @@ namespace elm { namespace genstruct {
 
 
 /**
- * @fn void SortedSLList::addFirst(const T item);
+ * @fn void SortedSLList::addFirst(const T& item);
  * Add the given item at the first position in the list.
  * @param item	Item to add.
  */
 
 
 /**
- * @fn void SortedSLList::addLast(const T item);
+ * @fn void SortedSLList::addLast(const T& item);
  * Add the given item at the last position in the list. Remark that this method is really inefficient.
  * Its working time is in O(n), n number of nodes in the list. Use it only with
  * small list or revert to more powerful data structures.
@@ -100,7 +109,7 @@ namespace elm { namespace genstruct {
 
 
 /**
- * @fn void SortedSLList::remove(const T item);
+ * @fn void SortedSLList::remove(const T& item);
  * Remove the given item from the list or just one if the list contains many
  * items equals to the given one. The item type T must support the equality /
  * inequality operators.
@@ -109,7 +118,7 @@ namespace elm { namespace genstruct {
 
 
 /**
- * @class SortedSLList::Iterator <elm/genstruct/SortedSLList.h>
+ * @class SortedSLList::Iterator
  * Iterator for generic single link-list.
  */
 
@@ -141,35 +150,5 @@ namespace elm { namespace genstruct {
  * @fn void SortedSLList::Iterator::next(void);
  * Go to the next item.
  */
-
-
-/**
- * @class SortedSLList::Editor <elm/genstruct/SortedSLList.h>
- * Implements an editor for the generic single-link list.
- */
-
-
-/**
- * @fn SortedSLList::Editor::Editor(SortedSLList& list);
- * Build an editor on the given list.
- * @param list	List to iterate on.
- */
-
-
-/**
- * @fn void SortedSLList::Editor::addAfter(T item);
- * Add an item after the current one.
- * @param item	Item to add.
- * @warning It is an error to call this method if the end of the list is
- * reached.
- */
-
-
-/**
- * @fn void SortedSLList::Editor::removeNext(void);
- * Remove the next item from the list.
- * @warning It is an error to call this method if the end of the list is
- * reached.
- */ 
 
 } } // elm::genstruct
