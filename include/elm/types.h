500f623f1246fa4b631bbec3824b840f7e9f3129
casse <devnull@localhost>
2008-02-14 10:59:48 +0000

    types.h renamed to type_info.h.

--- include/elm/types.h
+++ include/elm/types.h
@@ -1,9 +1,9 @@
 /*
  *	$Id$
- *	types module
+ *	type_info class shortcut
  *
  *	This file is part of OTAWA
- *	Copyright (c) 2007, IRIT UPS.
+ *	Copyright (c) 2007-08, IRIT UPS.
  * 
  *	OTAWA is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -22,90 +22,6 @@
 #ifndef ELM_TYPES_H
 #define ELM_TYPES_H
 
-#include <elm/string.h>
-
-namespace elm {
-
-// type_info trait
-template <class T> struct type_info {
-	enum { is_type = 0 };
-	enum { is_scalar = 0 };
-	enum { is_enum = 0 };
-	enum { is_class = 1 };
-	static inline CString name(void) { return T::_class.name(); }
-};
-typedef struct type_t {
-	enum { is_type = 1 };
-	enum { is_scalar = 0 };
-	enum { is_enum = 0 };
-	enum { is_class = 0 };
-} type_t;
-typedef struct scalar_t {
-	enum { is_type = 1 };
-	enum { is_scalar = 1 };
-	enum { is_enum = 0 };
-	enum { is_class = 0 };
-} scalar_t;
-template <> struct type_info<bool>: public scalar_t {
-	static inline CString name(void) { return "<bool>"; }	
-};
-template <> struct type_info<signed int>: public scalar_t {
-	static inline CString name(void) { return "int"; }
-	static const int max = 0x7fffffff;
-	static const int min = -0x80000000;
-};
-template <> struct type_info<unsigned int>: public scalar_t {
-	static inline CString name(void) { return "<unsigned>"; }
-};
-template <> struct type_info<char>: public scalar_t {
-	static inline CString name(void) { return "<char>"; }
-};
-template <> struct type_info<signed char>: public scalar_t {
-	static inline CString name(void) { return "<signed char>"; }
-};
-template <> struct type_info<unsigned char>: public scalar_t {
-	static inline CString name(void) { return "<unsigned char>"; }
-};
-template <> struct type_info<signed short>: public scalar_t {
-	static inline CString name(void) { return "<short>"; }
-};
-template <> struct type_info<unsigned short>: public scalar_t {
-	static inline CString name(void) { return "<unsigned short>"; }
-};
-template <> struct type_info<signed long>: public scalar_t {
-	static inline CString name(void) { return "<long>"; }
-};
-template <> struct type_info<unsigned long>: public scalar_t {
-	static const unsigned long max = 0xffffffff;
-	static const unsigned long min = 0;
-	static inline CString name(void) { return "<unsigned long>"; }
-};
-template <> struct type_info<signed long long>: public scalar_t {
-	static inline CString name(void) { return "<long long>"; }
-};
-template <> struct type_info<unsigned long long>: public scalar_t {
-	static inline CString name(void) { return "<unsigned long long>"; }
-};
-template <> struct type_info<float>: public scalar_t {
-	static inline CString name(void) { return "<float>"; }
-};
-template <> struct type_info<double>: public scalar_t {
-	static inline CString name(void) { return "<double>"; }
-};
-template <> struct type_info<long double>: public scalar_t {
-	static inline CString name(void) { return "<long double>"; }
-};
-template <> struct type_info<CString>: public type_t {
-	static inline CString name(void) { return "<cstring>"; }
-};
-template <> struct type_info<String>: public type_t {
-	static inline CString name(void) { return "<string>"; }
-};
-
-// Useful inlines
-inline int max(int x, int y) { return (x > y) ? x : y; }
-inline int min(int x, int y) { return (x < y) ? x : y; }
-
-} // elm
+#include <elm/type_info.h>
 
 #endif // ELM_TYPES_H
