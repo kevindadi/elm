690f7ce5ee38eca21fc4863c6ef6a2eb92e88327
casse <devnull@localhost>
2008-06-05 21:34:13 +0000

    Added new Tree implementation (inhstruct:: and genstruct::).

--- include/elm/genstruct/Tree.h
+++ include/elm/genstruct/Tree.h
@@ -1,29 +1,90 @@
 /*
- * $Id$
- * Copyright (c) 2006, IRIT-UPS.
+ *	$Id$
+ *	Tree class interface
  *
- * Interface for Tree class.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2008, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef ELM_GENSTRUCT_TREE_H
 #define ELM_GENSTRUCT_TREE_H
 
-#include <elm/Iterator.h>
+#include <elm/inhstruct/Tree.h>
 
 namespace elm { namespace genstruct {
 
-// TreeFacility class
-class TreeFacility {
-public:
-};
-
-
 // Tree class
-template <class T, class F = TreeFacility >
-class Tree: public F {
-	Tree<T> *_children, *_sibling;
-	T _value;
+template <class T>
+class Tree: private inhstruct::Tree {
 public:
-	inline Tree(const T& value);
+	inline Tree(const T& value): _value(value) { }
+	
+	// Accessors
+	inline Tree *children(void) const
+		{ return static_cast<Tree *>(inhstruct::Tree::children()); }
+	inline Tree *sibling(void) const
+		{ return static_cast<Tree *>(inhstruct::Tree::sibling()); }
+	bool hasChild(Tree *tree) const
+		{ return inhstruct::Tree::hasChild(tree); }
+	inline bool contains(Tree *tree) const { return hasChild(tree); }
+	inline int count(void) const
+		{ return inhstruct::Tree::count(); }
+	inline bool isEmpty(void) const
+		{ return inhstruct::Tree::isEmpty(); }
+	inline operator bool(void) const
+		{ return inhstruct::Tree::isEmpty(); }
+
+	// Iterator class
+	class Iterator: public PreIterator<Iterator, T> {
+	public:
+		inline Iterator(const Iterator& iter): it(iter.it) { }
+		inline Iterator(const Tree *tree): it(tree) { }
+		inline bool ended (void) const { return it.ended(); }
+		inline const T& item(void) const
+			{ return static_cast<Tree *>(it.item())->_value; }
+		inline void next(void) { it.next(); }
+	private:
+		inhstruct::Tree::Iterator it;
+	};
+
+	// Mutators
+	inline void prependChild(Tree *child)
+		{ inhstruct::Tree::prependChild(child); }
+	inline void appendChild(Tree *child)
+		{ inhstruct::Tree::appendChild(child); }
+	inline void add(Tree *child)
+		{ inhstruct::Tree::add(child); }
+	template <class TT> void addAll(const TT& coll)
+		{ inhstruct::Tree::addAll(coll); } 
+	inline void removeChild(Tree *child)
+		{ inhstruct::Tree::removeChild(child); }
+	inline void remove(Tree *child)
+		{ inhstruct::Tree::remove(child); }
+	inline void remove(const Iterator& iter)
+		{ removeChild(iter); }
+	template <class TT> void removeAll(const TT& coll)
+		{ inhstruct::Tree::removeAll(coll); } 
+	inline void clear(void)
+		{ inhstruct::Tree::clear(); }
+	
+private:
+	T _value;
+	
+
+/*public:
 	
 	// Accessors
 	inline const T& value(void) const { return _value; }
@@ -58,60 +119,55 @@ public:
 		inline const T& item(void) const { return cur->value(); }
 		inline bool ended(void) const { return cur; }
 		inline void next(void) { cur = cur->sibling(); }
-	};
+	};*/
 };
 
 // Tree inlines
-template <class T, class F>
-inline Tree<T, F>::Tree(const T& value)
-: _children(0), _sibling(0), _value(value) {
-}
-
-template <class T, class F>
-inline void Tree<T, F>::addChild(Tree<T> *tree) {
+/*template <class T>
+inline void Tree<T>::addChild(Tree<T> *tree) {
 	assert(!tree->_sibling);
 	tree->_sibling = _children;
 	_children = tree;
 }
 
-template <class T, class F>
-inline void Tree<T, F>::addSibling(Tree<T> *tree) {
+template <class T>
+inline void Tree<T>::addSibling(Tree<T> *tree) {
 	assert(!tree->_sibling);
 	tree->_sibling = _sibling;
 	_sibling = tree;
 }
 
-template <class T, class F>
-inline void Tree<T, F>::removeChild(void) {
+template <class T>
+inline void Tree<T>::removeChild(void) {
 	assert(_children);
 	Tree<T> *next = _children->_sibling;
 	_children->_sibling = 0;
 	_children = next;
 }
 
-template <class T, class F>
-inline void Tree<T, F>::removeSibling(void) {
+template <class T>
+inline void Tree<T>::removeSibling(void) {
 	assert(_sibling);
 	Tree<T> *next = _sibling->_sibling;
 	_sibling->_sibling = 0;
 	_sibling = next;
 }
 
-template <class T, class F>
-inline void Tree<T, F>::clear(void) {
+template <class T>
+inline void Tree<T>::clear(void) {
 	while(_children)
 		removeChild();
 }
 
-template <class T, class F>
-void Tree<T, F>::deleteAll(void) {
+template <class T>
+void Tree<T>::deleteAll(void) {
 	while(_children) {
 		Tree<T> *tree = _children;
 		removeChild();
 		tree->deleteAll();
 	}
 	delete this;
-}
+}*/
 
 } } // elm:genstruct
 
