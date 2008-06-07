a9395cddc1e937220c9591635a8ab8eab2850954
casse <devnull@localhost>
2008-06-07 16:38:11 +0000

    First version oif genstruct Tree using inhstruct Tree.

--- src/genstruct_Tree.cpp
+++ src/genstruct_Tree.cpp
@@ -41,16 +41,18 @@ namespace elm { namespace genstruct {
  */
 
 /**
- * @fn const T& Tree::value(void) const;
+ * @fn const T& Tree::data(void) const;
  * Get the value stored in the tree.
  * @return	Stored value.
  */
 
+
 /**
- * @fn T& Tree::value(void);
+ * @fn T& Tree::data(void);
  * Get assignable reference on the value stored in the tree.
  * @return	Stored value reference.
  */
+
 
 /**
  * @fn const Tree<T> *Tree::children(void) const;
@@ -58,11 +60,13 @@ namespace elm { namespace genstruct {
  * @return	First children.
  */
 
+
 /**
  * @fn Tree<T> *Tree::children(void)
  * Get list of children of the tree, that is, the first child.
  * @return	First children.
  */
+
 
 /**
  * @fn const Tree<T> *Tree::sibling(void) const;
@@ -70,39 +74,11 @@ namespace elm { namespace genstruct {
  * @return	Next sibling.
  */
 
+
 /**
  * @fn Tree<T> *Tree::sibling(void)
  * Get the next sibling of the current tree.
  * @return	Next sibling.
  */
 	
-	// Mutators
-/*	inline void addChild(Tree<T> *tree);
-	inline void addSibling(Tree<T> *tree);
-	inline void removeChild(void);
-	inline void removeSibling(void);
-	inline void clear(void);
-	inline void deleteAll(void);
-	
-	// Operators
-	inline operator bool(void) const { return _children; }
-	inline operator const T&(void) const { return value(); }
-	inline operator T&(void) { return value(); }
-	inline Tree<T>& operator=(const T& value) { _value = value; }
-	inline const T& operator*(void) const { return value(); }
-	inline T& operator*(void) { return value(); }
-	inline Tree<T>& operator+=(Tree<T> *tree);
-	inline Tree<T>& operator+=(Tree<T>& tree);
-	
-	// Iterator class
-	class Iterator: public PreIterator<Iterator, T> {
-		const Tree<T> *cur;
-	public:
-		inline Iterator(const Tree<T> *tree): cur(tree->children()) { }
-		inline const T& item(void) const { return cur->value(); }
-		inline bool ended(void) const { return cur; }
-		inline void next(void) { cur = cur->sibling(); }
-	};
-};*/
-
 } } // elm:genstruct
