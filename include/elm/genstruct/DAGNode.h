11d29f712c0eff8ff59f52426e6f68c73b30d0c0
casse <devnull@localhost>
2008-10-07 13:11:00 +0000

    Added integer named types. Added copyright information. Added autogerated collection. New full working AVL tree implementation. Vector.count() replaces length(). Vector.length() is deprecated. Added Vector.xxxAll() methods. Fixed file opening using System.xxxFile() function. Fixed RandomAccessStream. Cleanup in <elm/Iterator.h>.

--- include/elm/genstruct/DAGNode.h
+++ include/elm/genstruct/DAGNode.h
@@ -1,14 +1,29 @@
 /*
- * $Id$
- * Copyright (c) 2005, IRIT-UPS.
+ *	$Id$
+ *	DAGNode class interface
  *
- * DAGNode.h -- DAG interface.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2005-08, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef ELM_GENSTRUCT_DAGNODE_H 
 #define ELM_GENSTRUCT_DAGNODE_H
 
 #include <elm/assert.h>
-#include <elm/Iterator.h>
+#include <elm/PreIterator.h>
 #include <elm/genstruct/SLList.h>
 #include <elm/genstruct/HashTable.h>
 
