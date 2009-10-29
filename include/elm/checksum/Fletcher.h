7da915ef0ed07f60a3f91d3a54b6f35cceff8853
casse <devnull@localhost>
2009-10-29 16:11:57 +0000

    Added put() and copyright.

--- include/elm/checksum/Fletcher.h
+++ include/elm/checksum/Fletcher.h
@@ -1,9 +1,24 @@
 /*
- * $Id$
- * Copyright (c) 2007 IRIT-UPS <casse@irit.fr>
+ *	$Id$
+ *	Fletcher class interface
+ *
+ *	This file is part of OTAWA
+ *	Copyright (c) 2007, IRIT UPS.
  * 
- * checksum::Fletcher class interface
- */ 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 #ifndef ELM_CHECKSUM_FLETCHER_H
 #define ELM_CHECKSUM_FLETCHER_H
 
@@ -23,7 +38,6 @@ public:
 	Fletcher(void);
 	unsigned long sum(void);
 	void put(io::InStream& in);
-	void put(io::InStream& in, int length);
 	void put(const void *buffer, int length);
 	void put(const CString& str);
 	void put(const String& str);
