18cb85d292cfe70082378ec57ad60b8f531207c9
casse <devnull@localhost>
2010-03-01 13:00:39 +0000

    Transitive to new generation "option" module :   * new configuration system based on vararg and tags   * any name for options   * {Bool|String|Integer}Option will disappear   * ValueOption<T>, ListOption<T> and SwitchOption replacement   * position-dependent argument support.

--- test/test_option.cpp
+++ test/test_option.cpp
@@ -7,11 +7,10 @@
 
 #include <elm/util/test.h>
 #include <elm/options.h>
+#include <elm/option/BoolOption.h>
 
 using namespace elm;
 using namespace elm::option;
-
-Manager man;
 
 // EnumOption test
 typedef enum val_t {
@@ -26,20 +25,122 @@ EnumOption<int>::value_t vals[] = {
 	{ "three", three },
 	{ "", one }
 };
-EnumOption<int> enum_opt(man, 'e', "enum", "", vals);
+
+
+// manager initialization
+static const cstring program = "my-command",
+					 copyright = "copyright (c) 2010",
+					description = "a test application",
+					author = "H. Cassé <casse@irit.fr",
+					arg_free = "free argument";
+static const Version version(1, 2, 3);
 
 class MyCommand: public Manager {
 public:
-	MyCommand(void) {
-		version = "1.2.3";
+
+	MyCommand(void)
+	: Manager(
+		PROGRAM, &::program,
+		VERSION, new Version(::version),
+		COPYRIGHT, &::copyright,
+		DESCRIPTION, &::description,
+		AUTHOR, &::author,
+		ARG_FREE, &::arg_free,
+		END
+	) {
 	}
 };
+
+
+// options
+MyCommand man;
+BoolOption b(man, 'b', "boolean", "boolean test", false);
+EnumOption<int> enum_opt(man, 'e', "enum", "", vals);
+StringOption s(man, 's', "string", "string test", "", "");
+SwitchOption c(man, CMD, "command", END);
+SwitchOption sw(man, 'S', "switch option");
+
 
 // test_option()
 void test_option(void) {
 	CHECK_BEGIN("Option");
 
 	try {
+
+		// manager test
+		{
+			CHECK_EQUAL(man.getProgram(), program);
+			CHECK_EQUAL(man.getAuthor(), author);
+			CHECK_EQUAL(man.getDescription(), description);
+			CHECK_EQUAL(man.getCopyright(), copyright);
+			CHECK_EQUAL(man.getVersion(), version);
+			CHECK_EQUAL(man.getFreeArgumentDescription(), arg_free);
+		}
+
+		// boolean option
+		{
+			const char *argv[] = { "command", "-b", 0 };
+			FAIL_ON_EXCEPTION(OptionException, man.parse(2, argv));
+			CHECK_EQUAL(*b, true);
+			b = false;
+		}
+		{
+			const char *argv[] = { "command", 0 };
+			FAIL_ON_EXCEPTION(OptionException, man.parse(1, argv));
+			CHECK_EQUAL(*b, false);
+			b = false;
+		}
+		{
+			const char *argv[] = { "command", "--boolean", 0 };
+			FAIL_ON_EXCEPTION(OptionException, man.parse(2, argv));
+			CHECK_EQUAL(*b, true);
+			b = false;
+		}
+
+		// string option
+		{
+			cstring value = "hello, world !";
+			const char *argv[] = { "command", "--string", &value, 0 };
+			FAIL_ON_EXCEPTION(OptionException, man.parse(3, argv));
+			CHECK_EQUAL(*s, string(value));
+			s.set("");
+		}
+		{
+			cstring value = "hello, world !";
+			const char *argv[] = { "command", "--string=hello, world !", 0 };
+			FAIL_ON_EXCEPTION(OptionException, man.parse(2, argv));
+			CHECK_EQUAL(*s, string(value));
+			s.set("");
+		}
+		{
+			cstring value = "hello, world !";
+			const char *argv[] = { "command", "-s", &value, 0 };
+			FAIL_ON_EXCEPTION(OptionException, man.parse(3, argv));
+			CHECK_EQUAL(*s, string(value));
+			s.set("");
+		}
+		{
+			cstring value = "hello, world !";
+			const char *argv[] = { "command", "-s=hello, world !", 0 };
+			FAIL_ON_EXCEPTION(OptionException, man.parse(2, argv));
+			CHECK_EQUAL(*s, string(value));
+			s.set("");
+		}
+
+		// SwicthOption test
+		{
+			const char *argv[] = { "command", "-S", 0 };
+			FAIL_ON_EXCEPTION(OptionException, man.parse(2, argv));
+			CHECK_EQUAL(*sw, true);
+		}
+
+		// free command
+		{
+			const char *argv[] = { "command", "command", 0 };
+			FAIL_ON_EXCEPTION(OptionException, man.parse(2, argv));
+			CHECK_EQUAL(*c, true);
+		}
+
 		// Unknown option
 		{
 			const char *argv[] = { "command", "--bad", 0 };
@@ -65,12 +166,6 @@ void test_option(void) {
 				const char *argv[] = { "command", "-e", "four", 0 };
 				CHECK_EXCEPTION(OptionException, man.parse(3, argv));
 			}
-		}
-
-		// check StringOption
-		{
-			StringOption str(man, 's', "string", "", "", "ok");
-			CHECK(str);
 		}
 	}
 	catch(OptionException& e) {
