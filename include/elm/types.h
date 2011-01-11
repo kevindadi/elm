bb583f451539ca24d8e5d8d6f45ad0413ab39072
casse <devnull@localhost>
2011-01-11 08:58:06 +0000

    int types have moved in elm.int.h that also contains access to optimized integer specific functions.

--- include/elm/types.h
+++ include/elm/types.h
@@ -22,25 +22,6 @@
 #ifndef ELM_TYPES_H
 #define ELM_TYPES_H
 
-#include <stdint.h>
-
-namespace elm { namespace t {
-
-// base types
-typedef int8_t int8;
-typedef uint8_t uint8;
-typedef int16_t int16;
-typedef uint16_t uint16;
-typedef int32_t int32;
-typedef uint32_t uint32;
-typedef int64_t int64;
-typedef uint64_t uint64;
-
-// other types
-typedef uint32 size;
-typedef uint32 offset;
-typedef uint32 uint;
-
-} } // elm
+#include <elm/int.h>
 
 #endif // ELM_TYPES_H
