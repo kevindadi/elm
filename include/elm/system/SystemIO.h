d5fad00547cfc4163e555cdb2e8483f9f1ad76b5
casse <devnull@localhost>
2011-03-22 18:32:52 +0000

    Merged with Windows support.

--- include/elm/system/SystemIO.h
+++ include/elm/system/SystemIO.h
@@ -7,58 +7,64 @@
 #ifndef ELM_SYSTEM_SYSTEM_IO_H
 #define ELM_SYSTEM_SYSTEM_IO_H
 
-#if defined(__UNIX)
-#include <elm/io/UnixInStream.h>
-#include <elm/io/UnixOutStream.h>
+#include <elm/system/SystemIO.h>
+
+// Unix system streams
+#if defined(__unix)
+#	include <elm/io/UnixInStream.h>
+#	include <elm/io/UnixOutStream.h>
+
+	namespace elm { namespace system {
+
+	class SystemInStream: public io::UnixInStream {
+	public:
+		inline SystemInStream(int fd): UnixInStream(fd) { }
+	protected:
+		friend class System;
+	};
+
+	class SystemOutStream: public io::UnixOutStream {
+	public:
+		inline SystemOutStream(int fd): UnixOutStream(fd) { };
+	protected:
+		friend class System;
+	};
+
+	} }		// elm::system
+
+// Windows system streams
 #elif defined(__WIN32) || defined(__WIN64)
-#include <elm/io/WinInStream.h>
-#include <elm/io/WinOutStream.h>
+#	include <elm/io/WinInStream.h>
+#	include <elm/io/WinOutStream.h>
+
+	namespace elm { namespace system {
+
+	class SystemInStream: public io::WinInStream {
+	protected:
+		friend class System;
+	public:
+		inline SystemInStream(void* fd): WinInStream(WinInStream::fd()) { }
+		inline ~SystemInStream(void) { }
+	};
+
+	class SystemOutStream: public io::WinOutStream {
+	protected:
+		friend class System;
+	public:
+		inline SystemOutStream(void* fd): WinOutStream(WinOutStream::fd()) { }
+		inline ~SystemOutStream(void) { }
+	};
+
+	} }		// elm::system
+
+// unsupported OS
+#
+#	error "Unsupported OS !"
 #endif
 
-namespace elm { namespace system {
-
-// SystemInStream class
-#if defined(__UNIX)
-class SystemInStream: public io::UnixInStream {
-protected:
-	friend class System;
-	inline SystemInStream(int fd): UnixInStream(fd) { }
-	inline ~SystemInStream(void) { }
-};
-#elif defined(__WIN32) || defined(__WIN64)
-class SystemInStream: public io::WinInStream {
-protected:
-	friend class System;
-public:
-	inline SystemInStream(void* fd): WinInStream(WinInStream::fd()) { }
-	inline ~SystemInStream(void) { }
-};
-#endif
-
-
-// SystemOutStream class
-#if defined(__UNIX)
-class SystemOutStream: public io::UnixOutStream {
-protected:
-	friend class System;
-	inline SystemOutStream(int fd): UnixOutStream(fd) { };
-	inline ~SystemOutStream(void) { }
-};
-#elif defined(__WIN32) || defined(__WIN64)
-class SystemOutStream: public io::WinOutStream {
-protected:
-	friend class System;
-public:
-	inline SystemOutStream(void* fd): WinOutStream(WinOutStream::fd()) { }
-	inline ~SystemOutStream(void) { }
-};
-#endif
-
-
-} // system
 
 // Standard Streams
-namespace io {
+namespace elm { namespace io {
 
 extern system::SystemInStream& in;
 extern system::SystemOutStream& out;
