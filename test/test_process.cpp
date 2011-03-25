34cc41d881aa2920a1107c68cefa23968c42839b
lecourt <devnull@localhost>
2011-03-25 10:52:50 +0000

    Windows : Debugging md5 and process

--- test/test_process.cpp
+++ test/test_process.cpp
@@ -11,6 +11,8 @@
 #include <elm/io.h>
 #include <elm/io/OutFileStream.h>
 
+#include <windows.h>
+
 using namespace elm;
 using namespace elm::system;
 
@@ -18,22 +20,30 @@ using namespace elm::system;
 void test_process(void) {
 	CHECK_BEGIN("Process");
 
+#if defined (__unix)
 	ProcessBuilder builder("ls");
+#elif defined(__WIN32) || defined(__WIN64)
+	ProcessBuilder builder("C:\\Win16App\\MinGW\\bin\\gcc.exe");
+#endif
 	Pair<PipeInStream *, PipeOutStream *> pipes = System::pipe();
 	builder.setOutput(pipes.snd);
 	io::OutFileStream err_file("error.txt");
 	builder.setError(&err_file);
 	Process *process = builder.run();
+	cout << "yapuka : " << GetLastError() << io::endl;
 	delete pipes.snd;
 	CHECK(process);
+	cout << "yapuka : " << GetLastError() << io::endl;
 	io::Input input(*pipes.fst);
 	String line;
 	input >> line;
+	cout << "yapuka : " << GetLastError() << io::endl;
 	while(line) {
 		cout << "> " << line;
 		input >> line;
 	}
 	process->wait();
+	cout << "yapuka : " << GetLastError() << io::endl;
 	CHECK_EQUAL(process->returnCode(), 0);
 	
 	CHECK_END;
