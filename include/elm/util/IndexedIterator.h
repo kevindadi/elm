3f896cba2f94a9b5dc516366026e331262167b1e
casse <devnull@localhost>
2016-01-11 13:12:08 +0100

    util: small fix.

--- include/elm/util/IndexedIterator.h
+++ include/elm/util/IndexedIterator.h
@@ -4,7 +4,7 @@
  *
  *	This file is part of OTAWA
  *	Copyright (c) 2007-08, IRIT UPS.
- * 
+ *
  *	OTAWA is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
  *	the Free Software Foundation; either version 2 of the License, or
@@ -16,7 +16,7 @@
  *	GNU General Public License for more details.
  *
  *	You should have received a copy of the GNU General Public License
- *	along with OTAWA; if not, write to the Free Software 
+ *	along with OTAWA; if not, write to the Free Software
  *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef ELM_INDEXED_ITERATOR_H
@@ -31,12 +31,12 @@ template <class I, class T, class C>
 class IndexedIterator: public PreIterator<I, const T&> {
 	typedef IndexedIterator<I, T, C> this_t;
 public:
-	
-	inline IndexedIterator(const C& collection): c(&collection), i(0) { } 
-	inline IndexedIterator(const this_t& iter): c(iter.c), i(iter.i) { } 
-	inline this_t& operator=(const this_t& iter) { c = iter.c; i = iter.i; }
-	
-	inline bool ended(void) { return i >= c->size(); }
+
+	inline IndexedIterator(const C& collection): c(&collection), i(0) { }
+	inline IndexedIterator(const this_t& iter): c(iter.c), i(iter.i) { }
+	inline this_t& operator=(const this_t& iter) { c = iter.c; i = iter.i; return *this; }
+
+	inline bool ended(void) const { return i >= c->size(); }
 	inline const T& item(void) const { return (*c)[i]; }
 	inline void next(void) { i++; }
 
