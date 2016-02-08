1500ad7cffd25dc61f54bf694f367dbfacb68807
casse <devnull@localhost>
2016-02-08 09:58:12 +0100

    gen: cleanup in FragTable.

--- include/elm/genstruct/FragTable.h
+++ include/elm/genstruct/FragTable.h
@@ -32,26 +32,30 @@ template <class T> class FragTable {
 	genstruct::Vector<T *> tab;
 	int size, msk, shf, used;
 public:
-	inline FragTable(int size_pow = 8);
-	inline FragTable(const FragTable& tab) { addAll(tab); }
-	inline ~FragTable(void);
-	
+	inline FragTable(int size_pow = 8)
+		: size(1 << size_pow), msk(size - 1), shf(size_pow), used(size)
+		{ ASSERTP(size_pow > 0, "size must be greater than 0"); }
+
+	inline FragTable(const FragTable& tab)
+		: size(tab.size), msk(size - 1), shf(tab.shf), used(0)
+		{ addAll(tab); }
+
+	inline ~FragTable(void)
+		{ for(int i = 0; i < tab.count(); i++) delete [] tab[i]; }
+
 	// Collection concept
 	inline int count (void) const { return length(); }
 	inline bool isEmpty(void) const { return tab.count() == 0; }
  	inline operator bool (void) const { return !isEmpty(); }
 	inline bool contains(const T &item) const
-		{ for(int i = 0; i < length(); i++)
-			if(get(i) == item) return true; return false; } 
+		{ for(int i = 0; i < length(); i++) if(get(i) == item) return true; return false; }
 
 	class Iterator: public PreIterator<Iterator, const T&> {
 	public:
-		inline Iterator(const FragTable<T>& array, int pos = 0);
-		inline Iterator(const Iterator& iter)
-			: arr(iter.arr), i(iter.i), len(iter.len) { }
-		inline void next(void);
-		inline const T& item(void) const;
-		inline bool ended(void) const;
+		inline Iterator(const FragTable<T>& array, int pos = 0): arr(&array), i(pos), len(array.count()) { }
+		inline void next(void) { ASSERT(i < len); i++; }
+		inline const T& item(void) const { return arr->get(i); }
+		inline bool ended(void) const { return i >= len; }
 	protected:
 		friend class FragTable;
 		const FragTable<T> *arr;
@@ -59,8 +63,13 @@ public:
 	};
  	
 	// MutableCollection concept
-	inline void clear(void);
-	inline void add(const T &item);
+	inline void clear(void)
+		{	for(int i = 0; i < tab.count(); i++) delete [] tab[i];
+			tab.setLength(0); used = size; }
+
+	inline void add(const T &value)
+		{	if(used >= size) { tab.add(new T[size]); used = 0; }
+			tab[tab.length() - 1][used++] = value; }
 	template <template <class _> class C >
 	void addAll(const C<T> &items)
 		{ for(typename C<T>::Iterator i(items); i; i++) add(i); }
@@ -72,8 +81,9 @@ public:
 		{ for(typename C<T>::Iterator i(items); i; i++) remove(i); }
 
 	// Array concept
-	inline int length(void) const;
-	inline const T& get(int index) const;
+	inline int length(void) const { return ((tab.count() - 1) << shf) + used; }
+	inline const T& get(int index) const
+		{ ASSERTP(index >= 0 && index < length(), "index out of bounds"); return tab[index >> shf][index & msk]; }
 	inline int indexOf(const T &value, int start = 0) const
 		{ for(Iterator i(*this, start); i; i++)
 			if(i == value) return i.i; return -1; }
@@ -83,156 +93,42 @@ public:
 	inline const T& operator[](int index) const { return get(index); }
 	
 	// MutableArray concept
-	void shrink(int length);
-	inline void set(int index, const T &item);	
+	void shrink(int length)
+		{	ASSERTP(length < this->length(), "length too big"); int nl = (length + msk) >> shf;
+			for(int i = nl; i < tab.count(); i++) delete [] tab[i];
+			tab.setLength(nl); used = length & msk; if(!used) used = size;  }
+
+	inline void set(int index, const T &value)
+		{ ASSERTP(index >= 0 && index < length(), "index out of bounds"); tab[index >> shf][index & msk] = value; }
 	void set(const Iterator &iter, const T &item) { set(iter.i, item); }
-	inline T &get(int index);
+	inline T &get(int index)
+		{ ASSERTP(index >= 0 && index < length(), "index out of bounds"); return tab[index >> shf][index & msk]; }
 	inline T &operator[](int index) { return get(index); }
-	void insert(int index, const T &item);
+	void insert(int index, const T &item)
+		{ 	ASSERTP(index >= 0 && index <= length(), "index out of bounds");
+			int len = length(); alloc(1); for(int i = len - 1; i >= index; i--) set(i + 1, get(i)); set(index, item); }
+
 	inline void insert(const Iterator &iter, const T &item)
 		{ insert(iter.i, item); }
-	void removeAt(int index);
+
+	void removeAt(int index)  {
+		int len = length(); for(int i = index + 1; i < len; i++) set(i - 1, get(i));
+		used--; if(!used) { delete [] tab[tab.count() - 1]; tab.setLength(tab.count() - 1); used = size; }
+	}
+
 	inline void removeAt(const Iterator &iter) { removeAt(iter.i); }
-	
-	// Methods
-	int alloc(int count);
+
+	// other methods
+	int alloc(int count)
+		{	int res = length(); while(count >= size - used) { count -= size - used; tab.add(new T[size]); used = 0; }
+			used += count; return res; }
+
+	// operators
+	inline FragTable<T>& operator+=(T value) { add(value); return *this; }
 
 	// Deprecated
 	inline void clean(void) { clear(); }
-	inline FragTable<T>& operator+=(T value);	
 };
-
-
-// FragTable<T> Inlines
-template <class T>
-inline FragTable<T>::FragTable(int size_pow)
-: size(1 << size_pow), msk(size - 1), shf(size_pow), used(size) {
-	ASSERTP(size_pow > 0, "size must be greater than 0");
-}
-
-template <class T>
-inline FragTable<T>::~FragTable(void) {
-	for(int i = 0; i < tab.count(); i++)
-		delete [] tab[i];
-}
-
-template <class T>
-inline void FragTable<T>::clear(void) {
-	for(int i = 0; i < tab.count(); i++)
-		delete [] tab[i];
-	tab.setLength(0);
-	used = size;
-}
-
-template <class T>
-inline int FragTable<T>::length(void) const {
-	return ((tab.count() - 1) << shf) + used;
-}
-
-template <class T>
-inline const T& FragTable<T>::get(int index) const {
-	ASSERTP(index >= 0 && index < length(), "index out of bounds");
-	return tab[index >> shf][index & msk];
-}
-
-template <class T>
-inline T& FragTable<T>::get(int index) {
-	ASSERTP(index >= 0 && index < length(), "index out of bounds");
-	return tab[index >> shf][index & msk];
-}
-
-template <class T>
-inline void FragTable<T>::set(int index, const T& value) {
-	ASSERTP(index >= 0 && index < length(), "index out of bounds");
-	tab[index >> shf][index & msk] = value;
-}
-
-template <class T>	
-inline void FragTable<T>::add(const T& value) {
-	if(used >= size) {
-		tab.add(new T[size]);
-		used = 0;
-	}
-	tab[tab.length() - 1][used++] = value;
-}
-
-template <class T>
-inline FragTable<T>& FragTable<T>::operator+=(T value) {
-	add(value);
-	return *this;
-}
-
-template <class T>
-int FragTable<T>::alloc(int count) {
-	int res = length();
-	while(count >= size - used) {
-		count -= size - used;
-		tab.add(new T[size]);
-		used = 0;
-	}
-	used += count;
-	return res;
-}
-
-
-template <class T>
-void FragTable<T>::shrink(int length) {
-	ASSERTP(length < this->length(), "length too big");
-	int nl = (length + msk) >> shf;
-	for(int i = nl; i < tab.count(); i++)
-		delete [] tab[i];
-	tab.setLength(nl);
-	used = length & msk;
-	if(!used)
-		used = size; 
-}
-
-template <class T>
-void FragTable<T>::insert(int index, const T &item) {
-	ASSERTP(index >= 0 && index <= length(), "index out of bounds"); 
-	int len = length();
-	alloc(1);
-	for(int i = len - 1; i >= index; i--)
-		set(i + 1, get(i));
-	set(index, item);
-}
-
-
-template <class T>
-void FragTable<T>::removeAt(int index) {
-	int len = length();
-	for(int i = index + 1; i < len; i++)
-		set(i - 1, get(i));
-	used--;
-	if(!used) {
-		delete [] tab[tab.count() - 1];
-		tab.setLength(tab.count() - 1);
-		used = size;
-	}
-}
-
-
-// FragTable<T>::Iterator inlines
-template <class T>
-inline FragTable<T>::Iterator::Iterator(const FragTable<T>& array, int pos)
-: arr(&array), i(pos), len(array.count()) {
-}
-
-template <class T>
-inline void FragTable<T>::Iterator::next(void) {
-	ASSERT(i < len);
-	i++;
-}
-
-template <class T>
-inline const T& FragTable<T>::Iterator::item(void) const {
-	return arr->get(i);
-}
-
-template <class T>
-inline bool FragTable<T>::Iterator::ended(void) const {
-	return i >= len;
-}
 
 } } // elm::genstruct
 
