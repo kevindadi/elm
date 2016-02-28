864233181db261fb374644386c5ca38ea5b7493d
casse <devnull@localhost>
2016-02-28 10:55:54 +0100

    serial2: improved documentation.

--- src/serial2_serial.cpp
+++ src/serial2_serial.cpp
@@ -15,6 +15,305 @@ namespace elm { namespace serial2 {
 
 
 /**
+ * @defgroup serial Serialization / unserialization
+ *
+ * ELM provides support for pseudo-automatic serialization / unserialization
+ * of objects.
+ *
+ * The following C++ features are supported in serialization:
+ * @li scalar types (integer, floats)
+ * @li atomic types (strings),
+ * @li class type with inheritance,
+ * @li pointer and reference types with circularity detection,
+ * @li array types,
+ * @li ELM collection types,
+ * @li enumeration with user support for value names.
+ *
+ * In addition, the user can provides its own custom serialization code in
+ * a serializer / unserializer independent format.
+ *
+ * @par A simple example
+ *
+ * The serialization or the unserialization is a very simple to perform using
+ * the same interface as usual C++ input/output.
+ *
+ * @code
+ * #include <elm/serial2/serial.h>
+ * #include <elm/serial2/TextSerializer.h>
+ * #include <elm/serial2/collections.h>
+ *
+ * using namespace elm;
+ *
+ * int main(void) {
+ *	serial2::TextSerializer serializer;
+ *	serializer << "Hello, World !\n" << 666 << true;
+ *
+ * 	MyClass my_object;
+ *	serializer << my_object;
+ *
+ *	Vector<MyClass> objects;
+ *	for(int i = 0; i < 10; i++)
+ *		objects.add(my_object);
+ *	serializer << objects;
+ * }
+ * @endcode
+ *
+ * In the example above, we show how serializable objects (scalar values, string,
+ * MyClass and Vector) are easily serialized by a serializer providing a text
+ * output. As a default, the output is performed to the standard output. Notice
+ * that the unserialization of data from the bytes produced by this example must
+ * be performed in the same order.
+ *
+ * @code
+ * #include <elm/serial2/serial.h>
+ * #include <elm/serial2/XOMUnserializer.h>
+ * #include <elm/serial2/collections.h>
+ *
+ * using namespace elm;
+ *
+ * int main(void) {
+ *  serial2::XOMUnserializer unser("unser.xml");
+ *	string str;
+ *	int x;
+ *	bool boolean;
+ *	serializer >> str >> x >> boolean;
+ *
+ * 	MyClass my_object;
+ *	serializer >> my_object;
+ *
+ *	Vector<MyClass> objects;
+ *	serializer >> objects;
+ * }
+ * }
+ * @endcode
+ *
+ * The unserialization above looks much like the previous serialization
+ * example. It works in the same way as the input stream of C++ standard library.
+ * The read values must be put in variable. Notice that in the case of the Vector,
+ * the unserializer automatically allocate enough space to store the serialized
+ * collection of objects.
+ * @p
+ * This examples illustrates also the use of an XML unserializer taking its input
+ * from the file "unser.xml". This file is usual XML text that may be modified
+ * by hand and that contains very few serialization-systems items.
+ *
+ * ELM provides serializer / unserializer for usual types (like scalars, strings
+ * or data collections). Yet, to serializer custom classes, the user has to
+ * add some information about the fields to work with. In our example, we use
+ * the following declaration for MyClass in MyClass.h:
+ *
+ * @code
+ * #include <elm/serial2/macros.h>
+ * #include <elm/serial2/collections.h>
+ *
+ * class MyClass {
+ * 	SERIALIZABLE(MyClass, FIELD(name) & FIELD(value) & FIELD(attrs));
+ *	string name;
+ *	int value;
+ *	Vector<string> attrs;
+ * public:
+ *	...
+ * };
+ * @endcode
+ *
+ * The macro SERIALIZABLE is used to add to the class some RTTI information
+ * that is used by the serialization system. After the name of the class,
+ * this macro takes the list of fields to serialize separated by the '&' operator
+ * and embedded in the FIELD macro. The field macro is not mandatory, it allows
+ * only to provide better reading on textual output by providing the name of
+ * the field. This macro is used both by serialization and unserialization.
+ *
+ * @par Making a class serializable
+ *
+ * In ELM, making a class serializable is very easy. One has to add some
+ * RTTI information in the class declaration and add a macro in the class
+ * definition file.
+ *
+ * The declaration ".h" file must includes serialization headers:
+ * @code
+ * #include <elm/serial2/macros.h>
+ * #include <elm/serial2/collections.h>
+ * @endcode
+ *
+ * Then, we have to add the macro SERIALIZE in the class declaration including
+ * the name of the class and the list of the field to serialize separated by
+ * '&'.
+ * @code
+ * class ClassName {
+ *	SERIALIZABLE(ClassName, field1 & field2 & field3 & ...);
+ *	...
+ * };
+ * @endcode
+ *
+ * In the definition ".cpp" file, we have just to put the following macro that
+ * provides the implementation of the RTTI information of the class:
+ * @code
+ *	SERIALIZE(ClassName)
+ * @endcode
+ * The passed class name must be the same between the SERIALIZABLE and the SERIALIZE
+ * macro and fully qualified to avoid ambiguities.
+ *
+ * Declaring a field serializable is as easy as passing its name in the SERIALIZABLE
+ * list. Actually, a reference to the field is taken and used to read or write
+ * the serialized values. In human readable formats (like XML or text),
+ * it may be useful to provide also the identifier of the field to the
+ * serialization system. This is easily done using the FIELD macro in place
+ * of the field name:
+ * @code
+ *	SERIALIZE(MyClass, FIELD(field1) & field2 & FIELD(field3) & ...);
+ * @endcode
+ *
+ * Some serialization formats supports optional field definition. In this case,
+ * a default value may be provided with the DFIELD macro:
+ * @code
+ *	SERIALIZE(MyClass, DFIELD(field1, default_value) & field2 & FIELD(field3) & ...);
+ * @endcode
+ *
+ * Finally, if the serialized class inherit from a serializable class, the
+ * base class must be added to the list of field with the BASE macro:
+ * @code
+ * class MyBaseClass {
+ *	SERIALIZABLE(MyBaseClass, ...);
+ *	...
+ * };
+ *
+ * class MyClass: public MyBaseClass {
+ *	SERIALIZABLE(MyClass, BASE(MyBaseClass) & field1 & ...);
+ *	...
+ * };
+ * @endcode
+ *
+ * * @par Available serializers / unserializers
+ *
+ * ELM comes with some already implemented serializer / unserializers:
+ * @li @ref TextSerializer (serializer only).
+ * @li @ref XOMSerializer / @ref XOMUnserializer.
+ *
+ * More will be added as the library will evolve.
+ *
+ * @par Enumeration Serialization
+ *
+ * Depending on the serialization format (textual, XML, etc), it may be useful to
+ * provide more readable information for the human user. This applies typically
+ * to enumeration values. ELM provides already such a facility.
+ *
+ * In the code below, an enumeration type is declared (in header file) and *
+ * serialization information is provided by "ENUM" macro:
+ * @code
+ * 	typedef enum color_t {
+ *			RED,
+ *			GREEN,
+ *			BLUE
+ *		} color_t;
+ *		ENUM(color_t);
+ * @endcode
+ *
+ * Then, list of enumeration value list must be provided in the matching source file:
+ * @code
+ * 	ENUM_BEGIN(color_t)
+ *			VALUE(RED),
+ *			VALUE(GREEN),
+ *			VALUE(BLUE)
+ *		ENUM_END
+ * @endcode
+ *
+ * @par Collection serialization
+ *
+ * @par Customizing the serialization
+ *
+ * Basically, what does serialization is to pass forth and back field references to classes
+ * @ref Serializer for serialization and @ref Unserializer for unserialization. Depending
+ * on the operation, one of the following method is called:
+ * @code
+ * template <> void __serialize(Serializer& s, const T& v);
+ * template <> void __unserialize(Unserializer& s, T& v);
+ * @endcode
+ *
+ * With T being the type of the field to serialize. ELM provides serializer/unserializer for most
+ * basic types and most of its collection types. Providing __serialize() and __unserialize() with
+ * T being your own type is a first way to specialize the serialization process. In this configuration,
+ * you have to use methods provided by classes @ref Serializer and @ref Unserializer to perform
+ * the actual work.
+ *
+ * Then, one has to remark that the type T does not need to be the one of a field of serialized object.
+ * This may be any type that will refer to any object. This is the case of @ref FIELD macro that build an object
+ * of type @ref Field. The trick here is that the macro @ref SERIALIZABLE creates a specific method, named
+ * "__visit" in where the field are copied. This means that, when the field are built, a specific instance
+ * of the class is available and usable, for example, using the "this" self pointer. This means that at the
+ * construction of the field, the whole object (and its methods) are available and may be used for customizing
+ * the serialization process.
+ *
+ * Hence, to customize the way an object is serialized, one has to:
+ * @li design a dedicated class C,
+ * @li build an instance of class C passing possibly the "this" pointer or any reachable object resource,
+ * @li provide __serialize() and __unserialize() methods on this object.
+ *
+ * This is illustrated in the example below where methods getID and setID are used instead of the direct access
+ * to the field itself:
+ * @code
+ * class MyClass;
+ * class GetSetID {
+ * public:
+ * 		inline GetSetField(MyClass *p): _p(p) { }
+ *  	MyClass *_p;
+ * };
+ *
+ * class MyClass {
+ * public:
+ * 	SERIALIZABLE("my_class", GetSetID(this));
+ * };
+ *
+ * void __serialize(Serializer& s, const GetSet& i) {
+ * 		s.beginField("id");
+ *		s.onValue(i._p->getID());
+ *		s.endField();
+ *  }
+ *
+ * void __unserialize(Unserializer& s, const GetSet& i) {
+ * 		s.beginField("id");
+ * 		string s;
+ *		s.onValue(s);
+ *		i._p->setID(s);
+ *		s.endField();
+ *  }
+ * @endcode
+ *
+ * @par Writing a serializer
+ *
+ * Writing a serializer or an unserializer is relatively easy in ELM: they have just
+ * to implement either @ref Serializer interface or @ref Unserializer interfaces:
+ *
+ * Simple type (boolean, integer, float, string) are generated by a call to one of the
+ * onValue() method.
+ *
+ * Collection or array types starts by a call to beginCompound() and endCompound().
+ * For serializers, before each item, a call ton onItem() is performed. For unserializer,
+ * first a call to countItems() may be done to get the number of items in the compound.
+ * Then a call to nextItem() is performed before each value read (calls to onValue()).
+ *
+ * Object serialization / unserialization is surrounded by a beginObject() and endObject().
+ * The first parameter of the beginObject() contains a reference to the actual class
+ * of the object. Then each field is serialized / unserialized is surrounded by beginField()
+ * and endField() and the field value passed using other serialization methods.
+ *
+ * Pointer are a particular value for serialization / deserialization. First, circularities
+ * involved by the use of pointers must be supported. This means that the pointer to object
+ * must be stored in a map and when an already encountered object is referenced or serialized,
+ * a reference to it  must be serialized or the reference must be resolved to ensure
+ * that the same structure is rebuilt in memory. Notice that each object passed to onValue()
+ * takes a reference possibly dereferenced by "&" and that beginCompound() and beginObject()
+ * get a pointer on their object. The current implementation serialization / deserialization
+ * is not able to decide if an object is referenced or not by a pointer: either the serializer
+ * implementation has to provide an identifier for each serialized object, or the implementation
+ * has to provide a way to back patch with an identifier already serialized objects.
+ *
+ * Finally, one has to provide a special case for enumeration types with onEnum(). Notice that
+ * enumeration may be simply serialized as integers or, for user readable types, to use
+ * the provided enumeration type decription.
+ */
+
+
+/**
  * Storage of all available classes.
  */
 static HashTable<CString, AbstractType *> types; 
