dba6ad970312142505d451cbaec4175041394542
casse <devnull@localhost>
2016-04-18 14:55:25 +0200

    haskey: fix specific hashes.

--- include/elm/util/HashKey.h
+++ include/elm/util/HashKey.h
@@ -41,6 +41,38 @@ public:
 	static inline bool equals(const T& key1, const T& key2) { return &key1 == &key2 || Equiv<T>::equals(key1, key2); }
 };
 
+// Predefined hash keys
+template <> class HashKey<int> {
+public:
+	static inline t::hash hash(int key) { return t::hash(key); }
+	static inline bool equals(int key1, int key2) { return key1 == key2; }
+};
+
+template <> class HashKey<void *> {
+public:
+	static inline t::hash hash(void *key) { return t::hash(key); }
+	static inline bool equals(void *key1, void *key2) { return key1 == key2; }
+};
+
+template <> class HashKey<const void *> {
+public:
+	static inline t::hash hash(const void *key) { return t::hash(key); }
+	static inline bool equals(const void *key1, const void *key2)
+		{ return key1 == key2; }
+};
+
+template <> class HashKey<CString> {
+public:
+	static t::hash hash(CString key) { return hash_cstring(&key); }
+	static inline bool equals(CString key1, CString key2) { return key1 == key2; }
+};
+
+template <> class HashKey<String> {
+public:
+	static t::hash hash(const String& key) { return hash_string(key.chars(), key.length()); };
+	static inline bool equals(const String& key1, const String& key2) { return key1 == key2; };
+};
+
 };	// elm
 
 #endif	// ELM_UTIL_HASH_KEY_H
