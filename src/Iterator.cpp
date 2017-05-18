67f4a99b362a8f6edee46283559623b75f1c70ee
casse <devnull@localhost>
2017-05-18 11:14:52 +0200

    dyndata: header file fix.

--- src/Iterator.cpp
+++ src/Iterator.cpp
@@ -20,7 +20,7 @@
  *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include "../include/elm/dyndata/Iter.h"
+#include <elm/dyndata/Collection.h>
 
 namespace elm {
 
